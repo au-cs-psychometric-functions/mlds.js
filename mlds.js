175520786009694d523897127bd32ae33e5574af
Justice Suh <justice.suh@gmail.com>
2020-09-25 18:55:42 -0400

    Create link builder

--- mlds.js
+++ mlds.js
@@ -1,4 +1,47 @@
 (function(exports) {
+    const links = {
+        LOGIT: 'logit',
+        PROBIT: 'probit',
+        CAUCHY: 'cauchy',
+        LOG: 'log',
+        CLOGLOG: 'cloglog',
+    };
+
+    const logit = () => {
+
+    }
+
+    const probit = () => {
+
+    }
+
+    const cauchy = () => {
+
+    }
+
+    const log = () => {
+
+    }
+
+    const cloglog = () => {
+
+    }
+
+    const linkBuilder = function(link) {
+        switch (link) {
+            case links.LOGIT:
+                return logit();
+            case links.PROBIT:
+                return probit();
+            case links.CAUCHY:
+                return cauchy();
+            case links.LOG:
+                return log();
+            case links.CLOGLOG:
+                return cloglog();
+        }
+    }
+
     exports.test = function() {
         console.log('Hello World');
     }
