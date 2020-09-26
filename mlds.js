c288db9bd6c915460bda30d6c4681e94d1c2c613
Justice Suh <justice.suh@gmail.com>
2020-09-25 21:03:02 -0400

    Logit link

--- mlds.js
+++ mlds.js
@@ -8,7 +8,21 @@
     };
 
     const logit = () => {
-
+        const link = a => {
+            a = link.clean(a);
+            return map(a, e => log(e / (1 - e)));
+        }
+        link.clean = a => clip(a, Number.EPSILON, 1 - Number.EPSILON);
+        link.inverse = a => map(a, e => 1 / (1 + e));
+        link.derivative = a => {
+            a = link.clean(a);
+            return map(a, e => 1 / (e * (1 - e)))
+        }
+        link.inverseDerivative = a => {
+            a = exp(a);
+            return map(a, e => e / pow(1 + e, 2));
+        }
+        return link;
     }
 
     const probit = () => {
@@ -19,7 +33,7 @@
 
     }
 
-    const log = () => {
+    const logLink = () => {
 
     }
 
@@ -36,13 +50,24 @@
             case links.CAUCHY:
                 return cauchy();
             case links.LOG:
-                return log();
+                return logLink();
             case links.CLOGLOG:
                 return cloglog();
         }
     }
 
+    const map = (a, f) => a.map(e => f(e));
+
+    const clip = (a, aMin, aMax) => a.map(e => a < aMin ? aMin : e > aMax ? aMax : e);
+
+    const log = x => Math.log(x);
+
+    const exp = x => Math.exp(x);
+
+    const pow = (x, y) => Math.pow(x, y);
+
     exports.test = function() {
-        console.log('Hello World');
+        const logit = linkBuilder('logit');
+        console.log(logit([5]));
     }
 })(this);
