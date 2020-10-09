9f6dab739b4ff084e132d7c60cff50d427214b50
Justice Suh <justice.suh@gmail.com>
2020-10-08 22:21:18 -0400

    Clip functions

--- mlds.js
+++ mlds.js
@@ -1,34 +1,4 @@
 (function(exports) {
-    const GLM = function(y, x) {
-        console.log(x);
-        console.log(y);
-    }
-
-    exports.mlds = function(data) {
-        data = data.map(row1 => {
-            if (row[1] > row[3]) {
-                [row[1], row[3]] = row[3], row[1];
-                row[0] = 1 - row[0];
-            }
-            return row;
-        });
-
-        mx = Math.max.apply(null, data.map(row => Math.max.apply(Math, row)));
-        data = data.map(row => {
-            const arr = Array.from({length: mx}, () => 0);
-            arr[row[1] - 1] = 1;
-            arr[row[2] - 1] = -2;
-            arr[row[3] - 1] = 1;
-            arr[0] = row[0];
-            return arr;
-        });
-
-        y = data.map(row => (row[0]));
-        x = data.map(row => {
-            [, ...arr] = row;
-            return arr;
-        });
-
-        return GLM(y, x);
-    }
-})(this);
+    const default_clip = p => p.map(e => Math.max(Number.EPSILON, Math.min(1 - Number.EPSILON, e)));
+    const inf_clip = p => p.map(e => Math.max(Number.EPSILON, Math.min(Number.POSITIVE_INFINITY, e)))
+})();
